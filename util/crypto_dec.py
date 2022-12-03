201d56435d4bef98a8d28aa2daf7279669c76ea2
hayashi-mf <114895305+hayashi-mf@users.noreply.github.com>
2022-12-03 18:26:03 +0900

    暗号化使用文字列を環境変数で実装。チャンネル表示順変更

--- util/crypto_dec.py
+++ util/crypto_dec.py
@@ -1,13 +1,13 @@
 from Crypto.Cipher import AES
 import json
+import os
 
 class crypto_dec:
     def getdec():
-        with open('sectxt.json', 'rb') as f, open('k.txt', 'r') as k:
+        with open('sectxt.json', 'rb') as f:
             dec_text = f.read()
-            ke = json.loads(k.read())
-            iv = ke["a"].replace("rmfs","ncc8").replace("optg","4auy").replace("lqwt","").replace("uydfg","").replace("krsvn","").encode('utf-8')
-            key = ke["b"].replace("yql","0ie").replace("leh","fak").replace("hvd","").replace("kzr","").replace("iegwc","").encode('utf-8')
+            iv = os.getenv('ChatKeyA').replace("rmfs","ncc8").replace("optg","4auy").replace("lqwt","").replace("uydfg","").replace("krsvn","").encode('utf-8')
+            key = os.getenv('ChatKeyB').replace("yql","0ie").replace("leh","fak").replace("hvd","").replace("kzr","").replace("iegwc","").encode('utf-8')
             cipher2 = AES.new(key, AES.MODE_CBC, iv)
             decryption_text = cipher2.decrypt(dec_text)
             text = decryption_text[:-decryption_text[-1]].decode('utf-8')
